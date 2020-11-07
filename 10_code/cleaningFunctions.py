2a9f441fd74ed19adbc2e10c8534fa042c07dac4
Xiaohan Yang <xuy69@psu.edu>
2020-11-07 18:52:58 -0500

    Applied imputation on all comparison states

--- 10_code/cleaningFunctions.py
+++ 10_code/cleaningFunctions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import random
+import numpy as np
 
 # Function that cleans overdose mortality data
 # state_abbr must be capitalized
@@ -15,7 +17,7 @@ def clean_mortality(df, State_Abbr):
 
 
 # Function that cleans population data by state
-def clean_pop(df1,df2):
+def clean_pop(df1,df2,State_ABBR):
     df1 = df1.drop(["Unnamed: 1", 2000, 2001, 2002, "Unnamed: 12", "Unnamed: 13"], axis=1)
     df1 = df1.rename(columns={"Unnamed: 0": "County"})
     df2 = df2.drop(["Census", "Estimates Base", 2016, 2017, 2018, 2019], axis=1)
@@ -36,23 +38,21 @@ def clean_pop(df1,df2):
     df2 = df2.drop(["County", "State"], axis=1)
     df2 = df2.rename(columns={"County1": "County"})
     df_concat = pd.concat([df1,df2],ignore_index=True)
+    df_concat["State"] = State_ABBR
     return df_concat
 
 
 # Function that merges population data with mortality data
 def merge_mortalitypop(df_mortality, df_pop):
-    merged = df_mortality.merge(df_pop, how="left")
+    df_pop["County"] = df_pop["County"].astype("string")
+    df_pop["State"] = df_pop["State"].astype("string")
+    df_mortality["County"] = df_mortality["County"].astype("string")
+    df_mortality["State"] = df_mortality["State"].astype("string")
+    merged = df_mortality.merge(df_pop,left_on=["Year", "County"],
+                                right_on=["Year", "County"], how="right")
+    merged = merged.drop(["State_x"], axis=1)
+    merged = merged.rename(columns={"State_y": "State"})
+    merged["Deaths"] = merged["Deaths"].apply(lambda l: l if not np.isnan(l) else np.random.randint(0, 9))
     merged["Mortality Rate"] = merged["Deaths"] / merged["Population"]
     return merged
 
-
-# Function that groups dataframe by year, and calculate the total death and total population
-def calc_avg_mortality(df1, df2, df3):
-    df1 = df1.groupby('Year', as_index=False).sum()[['Year', 'Deaths', 'Population']]
-    df2 = df2.groupby('Year', as_index=False).sum()[['Year', 'Deaths', 'Population']]
-    df3 = df3.groupby('Year', as_index=False).sum()[['Year', 'Deaths', 'Population']]
-
-    df = pd.concat([df1, df2, df3], ignore_index=True)
-    df = df.groupby('Year', as_index=False).sum()
-    df['Avg Mortality Rate'] = df['Deaths'] / df['Population']
-    return df
